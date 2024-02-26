6c9fba8a00f9953ac4e0e75fae314fe7b6141f26
kjwojcik <157421197+kjwojcik@users.noreply.github.com>
2024-02-26 16:38:45 -0500

    Update windData.py

--- IceDragon/windData.py
+++ IceDragon/windData.py
@@ -1,5 +1,5 @@
-class windData():
-    def __init__(self, pressure, height, direction, speed, temp)
+class windData:
+    def __init__(self, pressure, height, direction, speed, temp):
         self.pressure = pressure
         self.height = height
         self.direction = direction
