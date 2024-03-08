6fa76ee17555199f265de0c8aace7704b854df36
kjwojcik <157421197+kjwojcik@users.noreply.github.com>
2024-03-08 17:19:21 -0500

    Delete windData.py

--- IceDragon/windData.py
+++
@@ -1,7 +0,0 @@
-class windData:
-    def __init__(self, pressure, height, direction, speed, temp):
-        self.pressure = pressure
-        self.height = height
-        self.direction = direction
-        self.speed = speed
-        self.temp = temp
