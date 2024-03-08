2b2212a6eba09f473355a55d3bfaa94a5d1f9b96
kjwojcik <157421197+kjwojcik@users.noreply.github.com>
2024-03-08 17:19:23 -0500

    Create windDataObject.py

---
+++ IceDragon/windDataObject.py
@@ -0,0 +1,7 @@
+class windData:
+    def __init__(self, pressure, height, direction, speed, temp):
+        self.pressure = pressure
+        self.height = height
+        self.direction = direction
+        self.speed = speed
+        self.temp = temp
